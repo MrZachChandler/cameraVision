06748c202f9a4178b87a7e81cccc9ab25f1765db
Zach Chandler <chandler@Zachs-MacBook-Pro.local>
2017-06-15 17:36:17 -0500

    Camera Detection Fun

---
+++ CameraDetection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CameraDetection
+//
+//  Created by Zach Chandler on 3/5/17.
+//  Copyright © 2017 Zach Chandler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
